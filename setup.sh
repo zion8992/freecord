9fea2f3c228111899b19aede5a77fe9c2ce730e5
zion8992 <private@email.address>
2026-08-08 13:44:53 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
-# Download and install nvm:
+# Download and install nvm
+# Copied from node website
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
 
-# in lieu of restarting the shell
+# restart the shell
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
 nvm install 24
 
+# install dependencies
 npm install @11ty/eleventy
 
+# run 11ty
 npx @11ty/eleventy
+
+# make the directory that pages will copy
+mkdir -p public
+
+# copy the 11ty out to public
+mv _site public
